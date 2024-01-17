27fea4b865bd7c2c4302f6d253bc12ed90f8e881
ygmypt <156908828+ygmypt@users.noreply.github.com>
2024-01-17 15:40:37 +0400

    Update module2.py

--- Часть2/module2.py
+++ Часть2/module2.py
@@ -1,17 +1,12 @@
 import numpy as np
 
-# Считывание числовой матрицы из файла
 number_matrix = np.loadtxt('C:\\temp\\numbermatrics.txt', delimiter=',')
 
-# Считывание единичной матрицы из файла
 dimension = int(np.sqrt(number_matrix.size))  # Определение размерности (предполагая, что это квадратная матрица)
 identity_matrix = np.loadtxt('C:\\temp\\firstmatrics.txt', delimiter=',').reshape((dimension, dimension))
 
-# Умножение матриц
 result_matrix = np.dot(number_matrix, identity_matrix)
 
-# Запись результата в файл
 np.savetxt('C:\\temp\\result_matrix.txt', result_matrix, delimiter=',', fmt='%d')
 
-# Вывод результата
 print(result_matrix)
