02d5f07fae2582ccca03ed421b3f6e03669a24d9
ygmypt <156908828+ygmypt@users.noreply.github.com>
2024-01-17 15:36:44 +0400

    Update module5.py

--- Часть1/module5.py
+++ Часть1/module5.py
@@ -1,18 +1,15 @@
-# Функция для поиска элемента в словаре
 def search_in_dictionary(data, key):
     if key in data:
         return data[key]
     else:
         return None
 
-# Функция для поиска элемента в списке
 def search_in_list(data, item):
     if item in data:
         return f"Элемент {item} найден в списке на позиции {data.index(item)}"
     else:
         return "Элемент не найден в списке"
 
-# Пример использования функций
 dictionary_data = {"name": "Alice", "age": 25, "city": "New York"}
 list_data = [10, 20, 30, 40, 50]
 
@@ -21,12 +18,10 @@ print(search_in_list(list_data, 30)) # Поиск элемента в списк
 
 from fuzzywuzzy import fuzz
 
-# Функция для выполнения нечеткого сравнения строк
 def fuzzy_string_compare(str1, str2):
     similarity_score = fuzz.ratio(str1, str2)
     return similarity_score
 
-# Пример использования функции
 string1 = "apple"
 string2 = "aple"
 score = fuzzy_string_compare(string1, string2)
